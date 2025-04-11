cf0407b258febc6516b318f00ac37df4ede88024
nan-park <nemyrmain@gmail.com>
2025-04-11 11:01:17 +0900

    Initial Commit

---
+++ ReTrack/ReTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReTrackApp.swift
+//  ReTrack
+//
+//  Created by 박난 on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ReTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReTrackTests/ReTrackTests.swift
@@ -0,0 +1,16 @@
+//
+//  ReTrackTests.swift
+//  ReTrackTests
+//
+//  Created by 박난 on 4/11/25.
+//
+
+import Testing
+
+struct ReTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
